ef758c3efa7be1c892b8b326a561e1157a7ebb35
Donato Borrello <donato@jwplayer.com>
2015-09-23 14:08:31 -0400

    Update helloworld.js

--- jw6-plugin-sdk/plugins/helloworld/helloworld.js
+++ jw6-plugin-sdk/plugins/helloworld/helloworld.js
@@ -1,23 +1,32 @@
 (function(jwplayer){
 
-var template = function(player, config, div) {
+// player - an instance of the player's public api
+// config - plugin options, searching the player config for an attribute matching the plugin name
+// div - an element where you can put your content
+var Plugin = function(player, config, div) {
 
     function setup(evt) {
         div.style.color = '#F00';
         div.style.padding = '8px';
+        
         if(config.text) {
             div.innerHTML = config.text;
         } else {
-            div.innerHTML = "Hello World!";
+            div.innerHTML = 'Hello World!';
         }
     };
+    
     player.onReady(setup);
 
-    this.resize = function(width, height) {
+    this.resize = function(width, height) { 
+        // do anything? nahh
     };
 
 };
 
-jwplayer().registerPlugin('helloworld', '6.0', template);
+var minPlayerVersion = '6.0';
+var pluginName = 'helloworld';
+
+jwplayer().registerPlugin(pluginName, minPlayerVersion, Plugin);
 
 })(jwplayer);
