a396e0cabe84dbe7781cf927d5deb5b366f18fbc
Rob Walch <robwalch@users.noreply.github.com>
2017-03-20 11:45:49 -0400

    Update external provider example timer.js

--- providers/timer.js
+++ providers/timer.js
@@ -1,25 +1,12 @@
 (function(jwplayer) {
-    
-    /*
-     * These objects are only available in commercial player builds.
-     * They are considered deprecated and not included in open-source
-     * builds of jwplayer/jwplayer.
-     *
-     * Open-source builds only expose underscore on player instances: `jwplayer()._`
-     *  events and state constants should be replaced with the appropriate strings.
-     *
-     * See:
-     * https://github.com/jwplayer/jwplayer/blob/v7.2.0/src/js/events/events.js
-     * https://github.com/jwplayer/jwplayer/blob/v7.2.0/src/js/events/states.js
-     */
-    
+
     var utils = jwplayer.utils,
         _ = jwplayer._,
         events = jwplayer.events,
         states = events.state,
-        
         TIMEOUT_MS = 25,
-        TIMEOUT_SEC = TIMEOUT_MS/1000;
+        TIMEOUT_SEC = TIMEOUT_MS/1000,
+        TIMER = 'timer';
 
 
     function formatTime(t) {
@@ -28,81 +15,122 @@
     }
 
 
-
-    function TimerProvider(/*_playerId, _playerConfig*/) {
-
+    function TimerProvider(/* _playerId */) {
+        this.name = TIMER;
+        this.startTime = new Date();
         this.position = 0;
-        this.duration = 18.0;
-        this._timerElement = document.createElement('div');
+        this.duration = 0;
+        var _container;
+        var _timerTimeout;
+        var _timerElement = document.createElement('div');
+        var _this = this;
 
-        utils.extend(this, new events.eventdispatcher('provider.' + this.name));
-
-        utils.css.style(this._timerElement, {
+        utils.style(_timerElement, {
             color : 'white',
-            'text-align': 'center',
+            textAlign: 'center',
             height: '100%',
-            'font-size' : '18px',
-            'margin-top': '25%'
+            fontSize : '18px',
+            marginTop: '25%'
         });
-    }
 
-    _.extend(TimerProvider.prototype, {
+        _.extend(this, jwplayer().Events, {
+            getName: TimerProvider.getName,
+            play: play,
+            load: load,
+            setContainer: setContainer,
+            stop: stop,
+            supportsFullscreen: _.constant(true),
+            setVisibility: setVisibility,
+            pause: pause,
+            seek: seek
+        });
 
-        load : function(item) {
-            // Item is an object containing, sources, starttime, and labels
+        function getCurrentTimeStr() {
+            return formatTime(_this.position);
+        }
+
+        function seek(time) {
+            this.position = Math.max(0, Math.min(time, this.duration));
+        }
+
+        function load(item) {
+            this.duration = item.duration || this.duration || 30;
             this.setState(states.BUFFERING);
 
             this.seek(item.starttime || 0);
 
             // Play begins after the buffer is full
-            this.sendEvent(events.JWPLAYER_MEDIA_BUFFER_FULL);
-        },
+            this.trigger(events.JWPLAYER_MEDIA_BUFFER_FULL);
+        }
 
-        play : function() {
+        function setVisibility(state) {
+            state = !!state;
+            _container.style.opacity = state ? 1:0;
+        }
+
+        function play() {
+
             this.setState(states.PLAYING);
+            this.setVisibility(true);
 
-            var _this = this;
             function updateTimer() {
                 _this.position += TIMEOUT_SEC;
-                _this._timerElement.textContent = formatTime(_this.position);
-                _this._timerTimeout = setTimeout(updateTimer, TIMEOUT_MS);
-                _this.sendEvent(events.JWPLAYER_MEDIA_TIME, {
+                if (_this.duration && _this.position >= _this.duration) {
+                    _endedHandler();
+                    return;
+                }
+                _timerElement.textContent = getCurrentTimeStr();
+                _timerTimeout = setTimeout(updateTimer, TIMEOUT_MS);
+                _this.trigger(events.JWPLAYER_MEDIA_TIME, {
                     position: _this.position,
                     duration: _this.duration
                 });
             }
 
             updateTimer();
-        },
-        pause: function() {
-            clearTimeout(this._timerTimeout);
-            this.setState(states.PAUSED);
-        },
-        seek: function(time) {
-            this.position = Math.max(0, Math.min(time, this.duration));
-        },
-        stop : function() {
-            clearTimeout(this._timerTimeout);
-            this.setState(states.IDLE);
-        },
-        // This is called when the provider is chosen to be used
-        setContainer : function(element) {
-            this._container = element;
-            this._container.appendChild(this._timerElement);
-        },
-        getContainer : function() {
-            return this._container;
-        },
-        supportsFullscreen: _.constant(true)
-    });
+        }
 
+        function _endedHandler() {
+            if (_this.state !== states.IDLE && _this.state !== states.COMPLETE) {
+                clearTimeout(_timerTimeout);
+                _this.trigger(events.JWPLAYER_MEDIA_BEFORECOMPLETE);
+                _playbackComplete();
+            }
+        }
+
+        function _playbackComplete() {
+            clearTimeout(_timerTimeout);
+            _this.setState(states.COMPLETE);
+            _this.trigger(events.JWPLAYER_MEDIA_COMPLETE);
+
+        }
+
+        function pause() {
+            clearTimeout(_timerTimeout);
+            this.setState(states.PAUSED);
+        }
+
+        function stop() {
+            clearTimeout(_timerTimeout);
+            this.setState(states.IDLE);
+        }
+
+        function setContainer(element) {
+            _container = element;
+            _container.appendChild(_timerElement);
+        }
+    }
 
     // Register provider
     TimerProvider.supports = function(item) {
         var type = item.type || utils.extension(item.file);
+        return (type === TIMER);
+    };
 
-        // Accept files with a ".timer" extension, or with timer as the explicit type
-        return (type === 'timer');
+    TimerProvider.getName = function() {
+        return {
+            name: TIMER
+        };
     };
 
     jwplayer.api.registerProvider(TimerProvider);
